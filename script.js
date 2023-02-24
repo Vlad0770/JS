55bf62958cf581e07da9e49c21889d1e1edbfff8
Vlad <gri.05.10.2000@gmail.com>
2023-02-24 19:20:40 +0200

    JS

---
+++ script.js
@@ -0,0 +1,13 @@
+let firstNumber;
+let secondNumber;
+
+firstNumber = prompt("Enter your first number");
+secondNumber = prompt("Enter your second number");
+
+alert(Number(firstNumber) + Number(secondNumber));
+
+alert(firstNumber - secondNumber)
+
+alert(firstNumber * secondNumber)
+
+alert(firstNumber / secondNumber)
