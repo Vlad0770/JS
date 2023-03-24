0eba25d46cfb119176f9711e9b150da4f084445e
Vlad <gri.05.10.2000@gmail.com>
2023-03-24 23:34:58 +0200

    Js

--- script.js
+++ script.js
@@ -1,38 +1,80 @@
-let firstNumber;
-      let secondNumber;
+let askNumber = function (text) {
+        let result = prompt(text);
 
-      firstNumber = prompt('Ведіть першу цифру');
-      secondNumber = prompt('Ведіть другу цифру');
+        return result;
+      };
 
-      if (firstNumber && secondNumber) {
+      let firstNumber = askNumber("Ведіть першу цифру");
+      let secondNumber = askNumber("Ведіть другу цифру");
+
+      const isNotEmpty = function (number) {
+        let result = Boolean(number);
+
+        return result;
+      };
+
+      const isFirstNumberNotEmpty = isNotEmpty(firstNumber);
+      const isSecondNumberNotEmpty = isNotEmpty(secondNumber);
+
+      function showMessage(a) {
+        console.log(a);
+      }
+
+      if (isFirstNumberNotEmpty && isSecondNumberNotEmpty) {
         firstNumber = Number(firstNumber);
         secondNumber = Number(secondNumber);
 
+        function checkIsFirstNumberLessThanSecondNumber(a, b) {
+          return a < b;
+        }
 
-        let isFirstNumberLessThanSecondNumber = firstNumber < secondNumber;
+        let isFirstNumberLessThanSecondNumber =
+          checkIsFirstNumberLessThanSecondNumber(firstNumber, secondNumber);
 
         if (isFirstNumberLessThanSecondNumber) {
-          let userAnswer = confirm('Ви впевнені?');
+          let userAnswer = confirm("Ви впевнені?");
+
+          const checkIsNumber = function (a) {
+            const isNumber = typeof a === "number";
+
+            return isNumber;
+          };
+
+          let minus = function (a, b) {
+            const isANumber = checkIsNumber(a);
+            const isBNumber = checkIsNumber(b);
+
+            if (isANumber && isBNumber) {
+              return a - b;
+            } else {
+              showMessage("One of the numbers is not a number");
+            }
+          };
 
           if (userAnswer === true) {
-            alert(firstNumber - secondNumber);
+            const firstNumberMinusSecond = minus(firstNumber, secondNumber);
+            showMessage(firstNumberMinusSecond);
           }
         } else {
-            alert(firstNumber - secondNumber);
-        }
+          const firstNumberMinusSecond = minus(firstNumber, secondNumber);
 
-        let isSecondNumberZero = secondNumber === 0;
+          showMessage(firstNumberMinusSecond);
+        };
+
+        const checkIsNumberEqualsToZero = function (a) {
+          return a === 0;
+        };
+
+        let isSecondNumberZero = checkIsNumberEqualsToZero(secondNumber);
 
         if (isSecondNumberZero) {
-            alert('Ділення не виконано, друге число є 0');
+          showMessage("Ділення не виконано, друге число є 0");
         } else {
-            alert(firstNumber / secondNumber);
+          showMessage(firstNumber / secondNumber);
         }
-
-        alert(firstNumber + secondNumber);
-
-        alert(firstNumber * secondNumber);
+        
+        showMessage(firstNumber + secondNumber);
+        showMessage(firstNumber * secondNumber);
       } else {
-        alert('Користувач відмовився вводити цифри');
-      }
-
+        showMessage("Користувач відмовився вводити цифри");
+      };
--- script2.js
+++ script2.js
@@ -1,39 +1,50 @@
-// let arr = [75,5465,5451,59,441,414,25,541,36,952];
+function getSum(arr) {
+    let result = 0;
 
-// let sum = 0;
-// let max;
-// let min;
-
-// for(let i = 0; i < arr.length; i++) {
-//     sum = sum + arr[i];
-// }
-
-// for(let i=0; i < arr.length; i++) {
-//     if( i === 0) {
-//         max = arr[i];
-//     } else if (max < arr[i]) {
-//         max = arr[i];
-//     }
-
-//     if( i === 0) {
-//         min = arr[i];
-//     } else if (min > arr[i]) {
-//         min = arr[i];
-//     }
-// }
-
-// console.log(sum);
-// console.log(max + "max");
-// console.log(min + "min")
-
-
-let lines = 5;
-let str = " ";
-let symbol = "#";
-for (let i = 0; i < lines; i++) {
-    str += symbol;
-    console.log(str);
+    for(let i = 0; i < arr.length; i++) {
+        result = result + arr[i];
+    }
+    return result;
 }
+
+function getMax(arr) {
+    let result;
+
+    for(let i=0; i < arr.length; i++) {
+        if( i === 0) {
+            result = arr[i];
+        } else if (result < arr[i]) {
+            result = arr[i];
+        }
+    }
+    return result;
+}
+
+function getMin(arr) {
+    let result;
+
+    for(let i=0; i < arr.length; i++) {
+        if( i === 0) {
+            result = arr[i];
+        } else if (result > arr[i]) {
+            result = arr[i];
+        }
+    }
+    return result;
+}
+
+let arr = [75,5465,5451,59,441,414,25,541,36,952];
+
+let sum = getSum(arr);
+let max = getMax(arr);
+let min = getMin(arr);
+
+console.log(sum + "sum");
+console.log(max + "max");
+console.log(min + "min")
+
+
+
 
 
 
